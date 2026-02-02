20c32fe9479ac0e1a6bc6d173845e8788d86c961
AlaaFayyad <cr905143@gmail.com>
2026-02-02 11:31:16 +0200

    Fix TypeScript JSX types for lottie-player

--- src/types/lottie.d.ts
+++ src/types/lottie.d.ts
@@ -1,18 +1,21 @@
 import type * as React from "react";
 
 declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "lottie-player": React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      > & {
-        src?: string;
-        background?: string;
-        speed?: string | number;
-        loop?: boolean;
-        autoplay?: boolean;
-      };
+  namespace React {
+    namespace JSX {
+      interface IntrinsicElements {
+        "lottie-player": React.DetailedHTMLProps<
+          React.HTMLAttributes<HTMLElement>,
+          HTMLElement
+        > & {
+          src?: string;
+          background?: string;
+          speed?: number | string;
+          loop?: boolean;
+          autoplay?: boolean;
+          style?: React.CSSProperties;
+        };
+      }
     }
   }
 }
