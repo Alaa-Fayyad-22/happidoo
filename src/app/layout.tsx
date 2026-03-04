4a39ee1cf2385b212a103253f581f6bbd574327b
AlaaFayyad <cr905143@gmail.com>
2026-03-04 14:22:36 +0200

    add speed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import SiteHeader from "@/components/SiteHeader";
 import SiteFooter from "@/components/SiteFooter";
 import { Analytics } from "@vercel/analytics/next";
 import WhatsAppButton from "@/components/WhatsAppButton";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   // <meta name="google-site-verification" content="GjFGs0_kz2ar_60uUIaatcH-Dw3qeETrU4Z6UCOfhoE" />
@@ -29,6 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
         {/* Vercel Web Analytics */}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
